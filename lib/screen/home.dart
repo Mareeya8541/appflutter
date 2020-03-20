68854cda0e664ba06d07c69427bb0ac2f6224940
[Mareeya] <[skninelsh.ya@rmutsvmail.com]>
2020-03-21 06:49:05 +0700

    appflutter

---
+++ lib/screen/home.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+   Widget showlogo(){
+    return Image.asset('images/money.png',width: 200,);
+  }
+
+  Widget showText(){
+    return Container(
+      child: Text(
+        'Welcom ',
+         style: TextStyle(
+           fontSize:50.0,
+           color:Colors.deepOrange,
+           fontWeight:FontWeight.bold,
+           fontFamily: 'Righteous-Regular'
+      ),
+      ),
+    );
+  }
+
+  Widget emailText(){
+    return Container(
+      width: 300.0,
+      child: TextFormField(
+        //controller: textEditController,
+        decoration: InputDecoration(
+          icon: Icon(Icons.people,
+          size: 30.0,
+          color: Colors.deepOrangeAccent,
+          ),
+          labelText: 'Username',
+          hintText: 'your@email.com'
+        ),
+      ),
+    );
+  }
+  
+  Widget passText(){
+    return Container(
+      width: 300.0,
+      child: TextFormField(
+        obscureText: true,
+        //controller: textEditController1,
+        decoration: InputDecoration(
+          icon: Icon(Icons.vpn_key,
+          size : 30.0,
+          color: Colors.deepOrangeAccent,
+          ),
+          labelText: 'Password',
+          hintText: 'more 8 charactor'
+        
+        ),
+      ),
+    );
+  }
+  Widget buttonlogin(){
+    return Container(
+      width: 300.0,
+      child: RaisedButton.icon(
+        color: Colors.deepOrangeAccent,
+        icon: Icon(Icons.account_circle ,
+        size: 30.0,
+      ),
+      label: Text('LOGIN',style: TextStyle(
+           fontSize:15.0,
+           color:Colors.black,
+           fontWeight:FontWeight.bold,
+           fontFamily: 'Righteous-Regular'
+      ),),
+      onPressed: (){
+        // var route = MaterialPageRoute(
+        //     builder: (BuildContext context) => SecondPage()
+        //   );
+        //   Navigator.of(context).push(route);
+      },
+    ),
+    );
+  }
+  Widget buttonrigis(){
+    return Container(
+      width: 300.0,
+      child: RaisedButton.icon(
+        color: Colors.deepOrangeAccent,
+        icon: Icon(Icons.person_add,
+        size: 30.0,
+      ),
+      label: Text('RIGISTOR',style: TextStyle(
+           fontSize:15.0,
+           color:Colors.black,
+           fontWeight:FontWeight.bold,
+           fontFamily: 'Righteous-Regular'
+      ),),
+      onPressed: (){
+        // var route = MaterialPageRoute(
+        //     builder: (BuildContext context) => RigisPage()
+        //   );
+        //   Navigator.of(context).push(route);
+      },
+    ),
+    );
+  }
+   Widget blocklogin(){
+    return Container(
+      decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      gradient: LinearGradient(
+                          colors: [Colors.lightBlue[400], Colors.tealAccent[400]])),
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+            child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[showText(),emailText(),passText(),buttonlogin(),
+              buttonrigis()],
+          ),
+          ),
+            
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(image: DecorationImage(image: AssetImage('images/backg.jpg'),fit: BoxFit.fill)),
+          child: Center(child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              showlogo(),
+              blocklogin()
+            ],
+          ),),
+        ),
+        ),
+    );
+  }
+}
